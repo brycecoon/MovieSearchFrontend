656095516e4816eab11b7de533b8a53b82c64ce9
brycecoon <112597142+brycecoon@users.noreply.github.com>
2024-11-22 19:43:08 -0700

    movieFacts content is there

--- src/Pages/FunMovieFacts.tsx
+++ src/Pages/FunMovieFacts.tsx
@@ -1,8 +1,161 @@
+import brainProcessing from "../assets/Images/brainProcessingMovie.jpg";
+import firstMovie from "../assets/Images/firstMovie.jpg";
+import firstMovieTheatre from "../assets/Images/firstMovieTheatre.jpg";
+import kellyGang from "../assets/Images/kellyGang.jpg";
+import movieBand from "../assets/Images/movieBand.jpg";
+import movieReel from "../assets/Images/movieReel.jpg";
+import oldCamera from "../assets/Images/oldCamera.jpg";
+import panningCamera from "../assets/Images/panningCamera.jpg";
+import titanic from "../assets/Images/titanic.jpg";
+import DLP from "../assets/Images/DLP.jpg";
 
 const FunMovieFacts = () => {
   return (
-    <div>FunMovieFacts</div>
-  )
-}
+    <>
+      <div>
+        <div>DID YOU KNOW</div>
+        <div className="text-4xl">1. WHEN WAS FILM INVENTED? THE 1890S</div>
+        <img src={firstMovie}></img>
+        <p>
+          Motion pictures date all the way back to the 1890s when the first
+          moving picture cameras were invented. However, the very first moving
+          picture – The Roundhay Garden Scene – was actually a product of the
+          1880s. In 1888, French inventor Louis Le Prince filmed his family
+          prancing around in a circle in a whopping two second clip. While that
+          seems insignificant now, it’s innovation is what would eventually lead
+          to commercialized cameras and motion pictures. Before the advancement
+          of the Hollywood scene, movies were somewhat boring. They started out
+          short and only included a single scene that was about a minute long.
+          They were typically silent - except...
+        </p>
+        <div className="text-4xl">
+          2. THE EARLIEST SHORT FILMS WERE SOMETIMES ACCOMPANIED BY BANDS
+        </div>
+        <img src={movieBand}></img>
 
-export default FunMovieFacts
+        <p>
+          What fun would it be sitting in a theatre while random, everyday
+          scenes scrolled by silently on a screen? Awkward. To make up for the
+          lack of sound in a film, sometimes a band would play live music while
+          the movie was playing. After all, who wants to hear someone going to
+          town on a bag of popcorn in a deaf theater?
+        </p>
+        <div className="text-4xl">
+          3. THE PANORAMA SHOT WAS DEVELOPED IN 1987
+        </div>
+        <img src={panningCamera}></img>
+
+        <p>
+          1987 is the year panning cameras were first used in film production,
+          meaning the pan shot, also known as the panorama shot, was invented
+          then. Before, cameras were stationary, so you had to move the entire
+          camera and tripod to get any kind of movement. This was a huge
+          advancement in film making and cinematography as an art form.
+        </p>
+        <div className="text-4xl">
+          4. EARLY CAMERAS FILMED AT 16 FRAMES PER SECOND (FPS)
+        </div>
+        <img src={oldCamera}></img>
+
+        <p>
+          {" "}
+          By today’s standards, a 16 frames per second speed is pretty slow. For
+          perspective, modern 35mm cameras film at 25 FPS. If you want your mind
+          blown, some modern video games are played at 250 FPS. But hey, you’ve
+          got to start somewhere, right?
+        </p>
+        <div className="text-4xl">
+          5. 13 FRAMES PER SECOND IS THE SLOWEST SPEED THE HUMAN BRAIN WILL
+          PROCESS IMAGES CONSECUTIVELY
+        </div>
+        <img src={brainProcessing}></img>
+
+        <p>
+          13 FPS is the minimum speed that the human brain needs in order to
+          process consecutive images as movement. Anything less than that and
+          the human brain will process each frame as a separate picture. 16 FPS
+          is pretty close to 13, which is why old movies look so choppy and
+          unnatural.
+        </p>
+        <div className="text-4xl">
+          6. THE FIRST FEATURE-LENGTH FILM WAS PRODUCED IN 1906
+        </div>
+        <img src={kellyGang}></img>
+
+        <p>
+          The Australian film, The Story of the Kelly Gang, was the first
+          feature length film in history. You can see the cast, box office, and
+          budget details on IMDB. It was over an hour long, and the reel length
+          was about 4,000 feet. To put that in perspective, a small 5-inch reel
+          of film holds up to 200 ft., a 6-inch holds 300 ft. and a 7-inch 400
+          ft. Depending on the size of the reel that, movie is housed in, that’s
+          at least 10 reels of 7-inch film and at most 20 reels of 5-inch film.
+          Imagine that! Crazy enough, it was almost lost forever, but a few
+          pieces of the The Story of Kelly Gang film surfaced in 1975, which
+          helped preserve some of the history-making movie.
+        </p>
+        <div className="text-4xl">
+          7. THE FIRST MOVIE THEATERS OPENED IN 1907
+        </div>
+        <img src={firstMovieTheatre}></img>
+
+        <p>
+          Before 1907, most movies were shown in traditional theaters or at
+          carnivals. With the advent of movie theaters, the films became the
+          main attraction themselves.
+        </p>
+        <div className="text-4xl">
+          8. A 1,000 FOOT LONG FILM WILL PRODUCE 11 MINUTES OF FOOTAGE AT 25 FPS
+        </div>
+        <img src={movieReel}></img>
+
+        <p>
+          A standard reel of film that runs at 25 FPS is 1,000 feet long. This
+          1,000 feet of film will produce about 11 minutes of footage. That
+          means that projectionists at movie theatres had to change reels many
+          times during a single motion picture to keep it going uninterrupted.
+          Unlike today, where everything is digital and automated.
+        </p>
+        <div className="text-4xl">
+          9. THE TITANIC MOVIE WAS 17.7 REELS LONG WHEN RELEASED
+        </div>
+        <img src={titanic}></img>
+
+        <p>
+          Titanic came out in 1997 when film reels were still the only way to
+          project a movie. With a run time of 3 hours and 15 minutes, each copy
+          of Titanic was 17.7 reels long. That means, at 25 FPS, it consisted of
+          over 17,700 feet of film. That’s over 3 miles for a single movie. For
+          reference, the Titanic was 883 feet long … that’s nearly 20 Titanic's
+          long.
+        </p>
+        <div className="text-4xl">
+          10. MOVIE THEATRES NOW USE DIGITAL LIGHT PROCESSING (DLP)
+        </div>
+        <img src={DLP}></img>
+
+        <p>
+          Most movie theaters these days use digital video projectors. The
+          technology is called DLP which stands for Digital Light Processing.
+          Since modern films are projected digitally, movie studios don’t ship
+          huge reels of film to the theaters anymore, which in reality is a
+          giant time and money save. Now, they just send the videos via the
+          internet, satellite, or hard drive. Another huge advancement in the
+          film industry! The improvements in film have brought many different
+          ways for individuals to capture and create their own movies and
+          memories over the years. Whether you have tapes, film, photos, or
+          audio recordings, Legacybox can help digitize your memories so they
+          can be enjoyed by generations to come.
+        </p>
+      </div>
+      <footer className="flex h-screen items-end">
+        These Facts come from{" "}
+        <a href="https://legacybox.com/blogs/analog/fun-facts-film-history?srsltid=AfmBOopBD6Mvxaq50HOsgwJQPWi2U0IZmfF4bQFROe4Leiy8IxuOGv9X">
+          legacybox.com
+        </a>
+      </footer>
+    </>
+  );
+};
+
+export default FunMovieFacts;
